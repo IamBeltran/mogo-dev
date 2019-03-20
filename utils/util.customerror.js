00b09f6d5e48d7988215c91f811792c7665f14bc
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-03-20 16:28:41 -0600

    feat: add module utils

---
+++ utils/util.customerror.js
@@ -0,0 +1,18 @@
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │  BUILD CLASS CUSTOM ERROR.                                                        │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+class CustomError extends Error {
+  constructor(
+    { name, message } = {
+      name: 'MY_CUSTOM_ERROR',
+      message: 'Default Message for custom error',
+    },
+  ) {
+    super(name, message);
+    this.name = name;
+    this.message = message;
+  }
+}
+
+//  ──[  EXPORT MODULE ]─────────────────────────────────────────────────────────────────
+module.exports = CustomError;
