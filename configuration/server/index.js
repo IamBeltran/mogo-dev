b460bc7a25506c74a1fb5205d99def2a54b4b7c6
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-03-20 16:19:11 -0600

    feat: add server configuration

---
+++ configuration/server/index.js
@@ -0,0 +1,21 @@
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │  DECLARATION OF CONSTANTS.                                                        │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ──[  DEFAULT CONSTANTS  ]────────────────────────────────────────────────────────────
+const DEFAULT_SERVER_PORT = 3001;
+const DEFAULT_SERVER_HOSTNAME = 'localhost';
+const DEFAULT_SERVER_BASE_URL = 'http://localhost:3001';
+const DEFAULT_SERVER_KEY_API = 'MabelPines565';
+
+//  ┌───────────────────────────────────────────────────────────────────────────────────┐
+//  │  MODULE OF CONFIGURATION  OF SERVER                                               │
+//  └───────────────────────────────────────────────────────────────────────────────────┘
+
+//  ──[  EXPORT MODULE  ]────────────────────────────────────────────────────────────────
+module.exports = {
+  SERVER_PORT: parseInt(process.env.SERVER_PORT, 10) || parseInt(DEFAULT_SERVER_PORT, 10),
+  SERVER_HOSTNAME: process.env.SERVER_HOSTNAME || DEFAULT_SERVER_HOSTNAME,
+  SERVER_BASE_URL: process.env.SERVER_BASE_URL || DEFAULT_SERVER_BASE_URL,
+  SERVER_KEY_API: process.env.SERVER_KEY_API || DEFAULT_SERVER_KEY_API,
+};
