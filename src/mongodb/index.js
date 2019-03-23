6f135eb408ba1e6e2faafeef0c9024addb5bfb5f
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-03-23 16:00:12 -0600

    feat: add seed in index

--- src/mongodb/index.js
+++ src/mongodb/index.js
@@ -6,3 +6,4 @@
 module.exports.connection = require('./connection/mongodb.connect');
 module.exports.collection = require('./collection');
 module.exports.manager = require('./manager/mongo.manager');
+module.exports.seed = require('./seed/mongodb.seed');
