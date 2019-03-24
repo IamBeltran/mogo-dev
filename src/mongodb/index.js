b6df9165bb62d0cf8ca3428ac8536865f6d548a5
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-03-24 00:00:07 -0600

    feat: add dump index for database

--- src/mongodb/index.js
+++ src/mongodb/index.js
@@ -7,3 +7,4 @@ module.exports.connection = require('./connection/mongodb.connect');
 module.exports.collection = require('./collection');
 module.exports.manager = require('./manager/mongo.manager');
 module.exports.seed = require('./seed/mongodb.seed');
+module.exports.dump = require('./dump');
