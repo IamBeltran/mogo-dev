591899d0bd24b0d55d68e733d0305cc3aaf2bb0b
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-03-25 22:22:53 -0600

    refactor: rename module

--- utils/index.js
+++ utils/index.js
@@ -4,7 +4,7 @@
 
 //  ──[  EXPORT MODULE  ]────────────────────────────────────────────────────────────────
 module.exports.auxiliary = require('./util.auxiliary');
-module.exports.colorConsole = require('./util.colorconsole');
+module.exports.colorconsole = require('./util.colorconsole');
 module.exports.CustomError = require('./util.customerror');
 module.exports.logger = require('./util.logger');
 module.exports.middleware = require('./util.middleware');
