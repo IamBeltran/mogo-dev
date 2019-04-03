704b5a1c425801626ea2a43a022e5abc13ca94d8
Victor Giovanni Beltrán Rodríguez <beltran-victor@outlook.com>
2019-04-02 22:45:06 -0600

    feat: add normalize port module

--- utils/util.auxiliary.js
+++ utils/util.auxiliary.js
@@ -1,22 +1,42 @@
+/* eslint-disable no-restricted-globals */
 //  ┌───────────────────────────────────────────────────────────────────────────────────┐
 //  │ REQUIRE MY-MODULES DEPENDENCIES.                                                  │
 //  └───────────────────────────────────────────────────────────────────────────────────┘
-const { infoMsg } = require('./util.colorconsole');
+const { blue } = require('./util.colorconsole');
 
 //  ┌───────────────────────────────────────────────────────────────────────────────────┐
 //  │ MODULE AUXILIARY.                                                                 │
 //  └───────────────────────────────────────────────────────────────────────────────────┘
 
 //  ──[ IS TYPE  ]───────────────────────────────────────────────────────────────────────
-function isType(obj) {
+function isType(value) {
   // eslint-disable-next-line prettier/prettier
   return ({}).toString
-    .call(obj)
+    .call(value)
     .match(/\s([a-z|A-Z]+)/)[1]
     .toLowerCase();
 }
+//  ──[ NORMALIZE PORT  ]────────────────────────────────────────────────────────────────
+function normalizePort(value) {
+  const port = parseInt(value, 10);
+  if (isNaN(port)) {
+    return value;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
 
 //  ──[ PROCESS BAR  ]───────────────────────────────────────────────────────────────────
+/**
+ *
+ * @name  processBar
+ * @description   function that returns a text string
+ * @param         {string} [label='REGISTRO']
+ * @param         {number} total  -
+ * @param         {number} value  -
+ */
 function processBar(label = 'REGISTRO', total, value) {
   let bar = null;
   const percent = value / total;
@@ -50,12 +70,13 @@ function processBar(label = 'REGISTRO', total, value) {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
   process.stdout.write(
-    `${infoMsg(`→ PROGRESS [`)} ${bar} ${infoMsg(`]`)} ${percentstring}% ${infoMsg(
+    `${blue(`PROGRESS [`)} ${bar} ${blue(`]`)} ${percentstring}% ${blue(
       `||`,
-    )} ${value}/${total} ${infoMsg(`${label}`)}`,
+    )} ${value}/${total} ${blue(`${label}`)}`,
   );
 }
 
 //  ──[ EXPORT MODULE  ]─────────────────────────────────────────────────────────────────
 module.exports.isType = isType;
+module.exports.normalizePort = normalizePort;
 module.exports.processBar = processBar;
